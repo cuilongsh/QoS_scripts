88603f4cb9f29d252c1bae077c194e6add22603b
Cui,Long <long.cui@intel.com>
2025-12-19 13:41:52 +0530

    update mlc core scan for SNC0 HT by HT ./mlc_core_scan.sh R for read core scan

--- mlc/mlc_core_scan.sh
+++ mlc/mlc_core_scan.sh
@@ -1,26 +1,24 @@
-
-#for core_count in {48,32,16,8,4,2}; do
-
-#GNR 40-79,280-319
 ##by HT scaling start
-echo 40 - 40
-mlc --loaded_latency -b1000M -R -t20 -T -k40 -d0| grep 00000 | awk '{print $3}'
+patn=$1
+
+echo 1 - 1
+mlc --loaded_latency -b600M -$patn -t20 -k1 -d0| grep 00000 | awk '{print $2,$3}' >>mlc_core_scaling_$patn.txt
 
 for core_count in $(seq 2 2 79);do
 
-start_1st_half=40
-start_2nd_half=280
+start_1st_half=1
+start_2nd_half=121
 
 end_1st_half=$(($core_count/2 -1+$start_1st_half))
 end_2nd_half=$(($end_1st_half + $start_2nd_half-$start_1st_half))
-#echo $start_1st_half - $end_1st_half,$start_2nd_half - $end_2nd_half
+echo $start_1st_half - $end_1st_half,$start_2nd_half - $end_2nd_half
 
 #HT by HT scaling
 #with both sibling cores
-mlc --loaded_latency -b1000M -R -t20 -T -k$start_1st_half-$end_1st_half,$start_2nd_half-$end_2nd_half -d0| grep 00000 | awk '{print $3}'
+mlc --loaded_latency -b600M -$patn -t20 -k$start_1st_half-$end_1st_half,$start_2nd_half-$end_2nd_half -d0| grep 00000 | awk '{print $2,$3}'>>mlc_core_scaling_$patn.txt
 #echo $start_1st_half - $(($end_1st_half+1)),$start_2nd_half - $end_2nd_half
 #add one more hyper thread 
-mlc --loaded_latency -b1000M -R -t20 -T -k$start_1st_half-$(($end_1st_half+1)),$start_2nd_half-$end_2nd_half -d0| grep 00000 | awk '{print $3}'
+mlc --loaded_latency -b600M -$patn -t20 -k$start_1st_half-$(($end_1st_half+1)),$start_2nd_half-$end_2nd_half -d0| grep 00000 | awk '{print $2,$3}'>>mlc_core_scaling_$patn.txt
 
 
 ##with physical core only
